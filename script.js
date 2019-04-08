6796c3424eeff4e71130bd14a136bc308866d804
Nikolay Zvyagin <nik.zvyagin@gmail.com>
2019-04-09 02:06:49 +0300

    Remove scripts to a separate file

---
+++ script.js
@@ -0,0 +1,170 @@
+var userInput = document.querySelector('#user-input');
+var addItem = document.querySelector('#add-item');
+var run = document.querySelector('#run-compare');
+var compare = document.querySelector('#compare');
+var one = document.querySelector('#one');
+var two = document.querySelector('#two');
+var first = 0;
+var second = 1;
+var itemsList = [];
+
+var itemCounter = document.createElement('div');
+itemCounter.classList.add('item-counter', 'mb-1', 'py-2');
+itemsPanel.query().insertAdjacentElement('beforebegin', itemCounter);
+var refreshCounter = function() {
+  itemCounter.innerHTML = 'Количество элементов: ' + itemsList.length;
+}
+
+var result = document.createElement('ol');
+result.classList.add('result', 'mb-1', 'py-2');
+
+var renderPairs = function() {
+  if(second < itemsList.length) {
+    one.innerHTML = itemsList[first].name;
+    two.innerHTML = itemsList[second].name;
+    second = second+1;
+  } else if (first < itemsList.length - 2) {
+    first = first + 1;
+    second = first + 1;
+    one.innerHTML = itemsList[first].name;
+    two.innerHTML = itemsList[second].name;
+    second = second+1;
+  } else if(first === itemsList.length - 2 && second === itemsList.length) {
+      compare.remove();
+      document.querySelector('.item-counter').remove();
+      document.querySelector('.variants-counter').remove();
+      run.remove();
+      userInput.remove();
+      addItem.remove();
+      itemsPanel.query().remove();
+      document.querySelector('#cancel').remove();
+      var resultList = itemsList.sort(compareScores);
+      for(i=0; i<resultList.length; i++) {
+        result.insertAdjacentHTML('beforeend', '<li>' + resultList[i].name + '</li>')
+      }
+      document.querySelector('.container').insertAdjacentElement('beforebegin', result);
+  }
+}
+
+var fact = function(x) {
+  z = x;
+  for(i=x-1; i>=2; i--) {
+    z = z * i;
+  }
+  return z;
+}
+
+var compareScores = function(itemOne, itemTwo) {
+  return  itemTwo.score - itemOne.score;
+}
+
+var variantsCounter = document.createElement('div');
+variantsCounter.classList.add('variants-counter', 'mb-1', 'py-2')
+itemsPanel.query().insertAdjacentElement('beforebegin', variantsCounter);
+var countVariants = function() {
+  if(itemsList.length === 1) {
+    variantsCounter.innerHTML = 'Количество вариантов: ' + 0;
+  } else if(itemsList.length === 2) {
+    variantsCounter.innerHTML = 'Количество вариантов: ' + 1;
+  } else {
+    variantsCounter.innerHTML = 'Количество вариантов: ' + (fact(itemsList.length))/(2*fact(itemsList.length - 2));
+  }
+}
+
+var onClick = function(evt) {
+  evt.preventDefault();
+  if(userInput.value) {
+    itemsList.push({name: userInput.value, score: 0});
+    var item = document.createElement('div');
+    item.innerHTML = '<span class="flex-grow-1"></span><button class="btn btn-danger deleteItem">Удалить</button>';
+    itemsPanel.query().insertAdjacentElement('beforeend', item);
+    item.classList.add('item', 'bg-light', 'mb-1', 'py-2', 'd-flex');
+    setId();
+    item.querySelector('span').innerHTML = userInput.value;
+    refreshCounter();
+    countVariants();
+    userInput.value = null;
+    userInput.focus();
+  } else {
+    userInput.focus();
+  }
+}
+
+var onEnter = function(evt) {
+  if(userInput.value && evt.keyCode === 13) {
+    itemsList.push({name: userInput.value, score: 0});
+    var item = document.createElement('div');
+    item.innerHTML = '<span class="flex-grow-1"></span><button class="btn btn-danger deleteItem">Удалить</button>';
+    itemsPanel.query().insertAdjacentElement('beforeend', item);
+    item.classList.add('item', 'bg-light', 'mb-1', 'py-2', 'd-flex');
+    setId();
+    item.querySelector('span').innerHTML = userInput.value;
+    refreshCounter();
+    countVariants();
+    userInput.value = null;
+    userInput.focus();
+  } else {
+    userInput.focus();
+  }
+}
+
+var onClickCompare = function(evt) {
+  if(evt.target.id != 'compare') {
+    itemsList.find(itm => itm.name === evt.target.innerHTML).score += 1;
+    renderPairs();
+  }
+}
+
+var onClickRun = function(evt) {
+  evt.preventDefault;
+  renderPairs();
+  var cancel = document.createElement('div');
+  cancel.innerHTML = '<button class="btn btn-danger mb-3" id="cancel">Назад к списку</button>';
+  document.querySelector('.input-field').insertAdjacentElement('afterend', cancel);
+  document.querySelector('#cancel').addEventListener('click', onCancel);
+}
+
+var onClickReset = function(evt) {
+  evt.preventDefault;
+  document.location.reload(true);
+}
+
+var onCancel = function(evt) {
+  evt.preventDefault;
+  first = 0;
+  second = 1;
+  one.innerHTML = '';
+  two.innerHTML = '';
+  for(i=0; i<itemsList.length; i++) {
+    itemsList[i].score = 0;
+  }
+  document.querySelector('#cancel').remove();
+}
+
+var onClickDelete = function(evt) {
+  if(/deleteItem/.test(evt.target.id)) {
+    evt.preventDefault;
+    var num = evt.target.id.replace('deleteItem-', '');
+    document.querySelector('#item-' + num).remove();
+    itemsList.splice(num, 1);
+    setId();
+    refreshCounter();
+    countVariants();
+  }
+}
+
+var setId = function() {
+  var items = document.querySelectorAll('.item ');
+  for(i=0; i<items.length; i++) {
+    items[i].setAttribute('id', 'item-' + i);
+    items[i].querySelector('.deleteItem').setAttribute('id', 'deleteItem-' + i);
+  }
+}
+
+userInput.focus();
+addItem.addEventListener('click', onClick);
+document.querySelector('#user-input').addEventListener('keydown', onEnter);
+run.addEventListener('click', onClickRun);
+compare.addEventListener('click', onClickCompare);
+document.querySelector('#reset').addEventListener('click', onClickReset);
+itemsPanel.query().addEventListener('click', onClickDelete);
