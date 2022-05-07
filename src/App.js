c2e0b47918f1153462b9858253652ce28d92f5fb
Nikolay Zvyagin <nik.zvyagin@gmail.com>
2022-05-07 16:21:59 +0300

    Switch index template to React

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
 function App() {
-  return <div>Hello from React!</div>;
+  return (
+    <>
+      <a href="https://github.com/nzvyagin/compupler">
+        <img src="./forkme_right_orange_ff7600.svg" className="position-absolute" style={{ top: 0, right: 0 }} alt="Fork me on GitHub"/>
+      </a>
+      <main className="container pt-5">
+        <h1 className="text-center w-100 mt-5 mb-5">Compupler</h1>
+      </main>
+    </>
+  );
 }
 
 export default App;
