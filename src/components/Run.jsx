02ed870b8d1f0277b5c4e513eba5fe06f3b6d0d9
Nikolay Zvyagin <nik.zvyagin@gmail.com>
2022-08-26 00:14:57 +0300

    Delete unused id

--- src/components/Run.jsx
+++ src/components/Run.jsx
@@ -8,7 +8,7 @@ export const Run = ({entries, getPairs, setResultList}) => {
 
   return (
     <div className="d-flex flex-row justify-content-center">
-      <button id="runButton" onClick={handleRunCompare} className="btn btn-outline-success mb-5">Сравнить сочетания ({pairsCount})</button>
+      <button onClick={handleRunCompare} className="btn btn-outline-success mb-5">Сравнить сочетания ({pairsCount})</button>
     </div>
   );
 };
