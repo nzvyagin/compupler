56693a3a48755308cd09533ba424680c936a31ef
Nikolay Zvyagin <nik.zvyagin@gmail.com>
2022-10-24 18:05:50 +0300

    Fix trouble with deleting entry by pressing it

--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -1,10 +1,10 @@
 export const Entry = ({id, text, setEntries, entries}) => {
-  const handleEntryDelete = (e) => setEntries(entries.filter((entry) => entry.id !== e.currentTarget.id));
+  const handleEntryDelete = (e) => setEntries(entries.filter((entry) => entry.id !== e.target.parentNode.id));
 
   return (
-    <div id={id} onClick={handleEntryDelete} className="mb-2 d-flex align-items-center bg-light">
+    <div id={id} className="mb-2 d-flex align-items-center bg-light">
       <div className="flex-grow-1 mx-1 overflow-hidden text-nowrap">{text}</div>
-      <button className="btn btn-danger btn-sm">Удалить</button>
+      <button onClick={handleEntryDelete} className="btn btn-danger btn-sm">Удалить</button>
     </div>
   );
 };
